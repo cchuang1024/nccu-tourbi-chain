9f1472a4f0287000562cb0d2b49f438c8e017c54
cchuang1024 <cchuang.1024@zoho.com>
2020-01-09 04:09:31 +0800

    fix web ui

---
+++ tourbillon.py/README.md
@@ -0,0 +1,37 @@
+# TourbiChain - 時戳 oracle 服務
+
+## Module Toubillon - 服務方
+
+### 啟動服務
+
+#### 1. ganache-cli
+
+```
+./run-ganache.sh
+```
+
+#### 2. tourbillon
+
+```
+./run-tourbillon.sh
+```
+
+### 使用者界面
+
+[Index](http://127.0.0.1:5000/)
+
+> UI 開啟後會自動更新，因此無須使用 F5 更新畫面
+
+### API (開發中)
+
+[API](http://127.0.0.1:5000/tourbillon)
+
+## Module VeriChain - 驗證方
+
+### 執行驗證
+
+#### 1. veri-chain
+
+```
+./veri-treasure.sh
+```
--- tourbillon.py/tourbillon/surface/record.py
+++ tourbillon.py/tourbillon/surface/record.py
@@ -6,11 +6,11 @@ import tourbillon
 from tourbillon import app
 
 NAME_ADDRESS = \
-    m().set('1d259e3852419ddf6c75c6db2aa751df7b7e9319', 'Alice') \
-        .set('19e74a1813c73291c97f271bfae3a947eb385eac', 'Bob') \
-        .set('94020a8513391af84cfa320e56c13f0ca724d427', 'Cloe') \
-        .set('b450c1d01fe18e9d278255b5a240bc9deb3a0664', 'Devil') \
-        .set('534eb986dd72630ecfc494eb2843f31496a65d9b', 'Elijah')
+    m().set('0x1d259e3852419ddf6c75c6db2aa751df7b7e9319', 'Alice') \
+        .set('0x19e74a1813c73291c97f271bfae3a947eb385eac', 'Bob') \
+        .set('0x94020a8513391af84cfa320e56c13f0ca724d427', 'Cloe') \
+        .set('0xb450c1d01fe18e9d278255b5a240bc9deb3a0664', 'Devil') \
+        .set('0x534eb986dd72630ecfc494eb2843f31496a65d9b', 'Elijah')
 
 ViewModel = namedtuple('ViewModel', ['record_time', 'user', 'evidence', 'operation'])
 
@@ -28,5 +28,5 @@ def get_all(queue, vector):
 
 
 def build_model(tsa):
-    vm = ViewModel(tsa.timestamp, NAME_ADDRESS[tsa.address], tsa.digest, '存證')
+    vm = ViewModel(tsa.timestamp, NAME_ADDRESS[tsa.address.lower()], tsa.digest, '存證')
     return vm._asdict()
